8b6596d450f3d091329c4938ef8db2baa041467f
insan2121 <gamersg801@gmail.com>
2022-02-18 08:15:19 +0700

    remove readme

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -150,7 +150,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f4c41ec663897f02"; }
+/******/ 		__webpack_require__.h = function() { return "2495a7115cada412"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.a20f7fea21f7e74b.hot-update.js
+++ .next/static/webpack/webpack.a20f7fea21f7e74b.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "f4c41ec663897f02"; }
+/******/ 	__webpack_require__.h = function() { return "9dfe1bfb2aa85767"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
